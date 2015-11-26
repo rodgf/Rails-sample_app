03c171f8496f3357f7828166ab83cc61d1709c02
rodgf <rodgf@yahoo.com>
2015-11-26 11:04:09 -0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'static_pages/home'
   get 'static_pages/help'
-
+  get 'static_pages/about'
+    
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
