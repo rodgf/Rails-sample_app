3125b7b1fd515874e18b1516ff1a2aa6fdae3d0f
rodgf <rodgf@yahoo.com>
2015-12-08 17:51:35 -0200

    Agora com senha criptografada

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208123742) do
+ActiveRecord::Schema.define(version: 20151208190309) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20151208123742) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "password"
-    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
