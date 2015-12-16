b9a9f9978afc0d67fb0ec8173fa24be034ed1bcc
rodgf <rodgf@yahoo.com>
2015-12-15 23:01:16 -0200

    UserIndex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 	
   def index
@@ -24,7 +24,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+	
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+	
   def edit
     @user = User.find(params[:id])
   end
@@ -60,6 +66,6 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+			redirect_to(root_url) unless current_user?(@user) || current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211160954) do
+ActiveRecord::Schema.define(version: 20151216001953) do
+
   create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "password_digest"
-    t.string "remember_digest"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+
 end
