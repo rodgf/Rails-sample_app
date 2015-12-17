47a80c2c9f720a3fbf1e667e58a4e5f32e1a1f16
rodgf <rodgf@yahoo.com>
2015-12-17 16:33:26 -0200

    account_activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-    root 'application#inicio'
+	root 'application#inicio'
 
-    get 'home' => 'static_pages#home'
-    get 'help' => 'static_pages#help'
-    get 'about' => 'static_pages#about'
-    get 'contact' => 'static_pages#contact'
+	get 'home' => 'static_pages#home'
+	get 'help' => 'static_pages#help'
+	get 'about' => 'static_pages#about'
+	get 'contact' => 'static_pages#contact'
 
-    get 'signup' => 'users#new'
-		get 'login' => 'sessions#new'
-		post 'login' => 'sessions#create'
-		delete 'logout' => 'sessions#destroy'
+	get 'signup' => 'users#new'
+	get 'login' => 'sessions#new'
+	post 'login' => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
 
-		resources :users 
+	resources :users 
+  resources :account_activations, only: [:edit]
 end
